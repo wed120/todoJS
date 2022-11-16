b62438327d58664e0b262fed33d7e1d12da3a187
wed120 <wed120@mail.ru>
2022-11-16 16:20:20 +0300

    todo 1

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// test
+'use strict'
+
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const todo = document.querySelector('#todo');
+
+form.addEventListener('submit',function (event) {
+    event.preventDefault();
+
+    const taskText =taskInput.value;
+
+    const taskHtml = `<div class="todo">
+        <div class="galochka">
+            <img src="image/galochka.svg" alt="">
+        </div>
+        <div class="todoshka">
+            <h2>${taskText}</h2>
+        </div>
+        <div class="krestik">
+            <img src="image/krestik.svg" alt="">
+        </div>
+    </div>`;
+
+    todo.insertAdjacentHTML('beforeend', taskHtml)
+
+    taskInput.value = ''
+    taskInput.focus()
+})
