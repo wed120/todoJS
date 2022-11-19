039e30c9635994436f5bc1e91382972c407ab808
wed120 <wed120@mail.ru>
2022-11-19 16:19:07 +0300

    dev

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-'use strict'
-
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
-const todo = document.querySelector('#todo');
+const tasksLit = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
 
-form.addEventListener('submit',function (event) {
+
+//Добавление задачи
+form.addEventListener('submit', addTask);
+
+//Удаление задачи
+tasksLit.addEventListener('click', deleteTask);
+
+//Отмечаем задачу завершенной
+tasksLit.addEventListener('click', doneTask);
+
+//Функции
+function addTask(event) {
+    //Отменяем отправку формы
     event.preventDefault();
 
-    const taskText =taskInput.value;
+    //Достаем текст из поля ввода
+    const taskText = taskInput.value;
 
-    const taskHtml = `<div class="todo">
-        <div class="galochka">
-            <img src="image/galochka.svg" alt="">
+    //Формируем разметку для новой задачи
+    const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
+            <span class="task-title">${taskText}</span>
+            <button type="button" data-action="done" class="btn-action">
+            <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+            <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
         </div>
-        <div class="todoshka">
-            <h2>${taskText}</h2>
-        </div>
-        <div class="krestik">
-            <img src="image/krestik.svg" alt="">
-        </div>
-    </div>`;
+    </li>`
 
-    todo.insertAdjacentHTML('beforeend', taskHtml)
+    //Добавляем задачу на страницу
+    tasksLit.insertAdjacentHTML('beforeend', taskHTML);
 
-    taskInput.value = ''
+    //Очищаем поле ввода и возвращаем на него фокус
+    taskInput.value = '';
     taskInput.focus()
-})
+
+    //Проверка, если больше одной задачи, блок скрыт
+    if (tasksLit.children.length > 1) {
+        emptyList.classList.add('none')
+    }
+
+    //
+}
+
+function deleteTask(event) {
+
+    //Проверяем клик по кнопке
+    if (event.target.dataset.action === 'delete') {
+        const parentNode = event.target.closest('.list-group-item');
+        parentNode.remove();
+    }
+
+    //Проверяем, если нет задач показывать пустой список
+    if (tasksLit.children.length === 1) {
+        emptyList.classList.remove('none')
+    }
+}
+
+function doneTask(event) {
+
+    //Проверка, что клик по кнопке "задача выполнена"
+    if (event.target.dataset.action === 'done') {
+        const parentNode = event.target.closest('.list-group-item');
+        const taskTitle = parentNode.querySelector('.task-title');
+        taskTitle.classList.toggle('task-title--done')
+    }
+}
